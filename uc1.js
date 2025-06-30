599be593ce2c2a1f1b737aa7f84d8ac4d09785ab
ASWINIMK <aswinimk07@gmail.com>
2025-06-30 14:21:32 +0530

    game

---
+++ uc1.js
@@ -0,0 +1,11 @@
+// UC1 start position 0
+
+let numPlayers = 3;
+let player = [];
+let currentPlayer = 0;
+let rolls = [];    // UC6 count rolls
+
+for(let i = 0; i < numPlayers; i++){
+    player[i] = 0;
+    rolls[i] = 0;
+}
